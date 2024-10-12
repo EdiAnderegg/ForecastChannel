f4d716a0ceb80f305a40bb25af1762f8dc802f97
EdiAnderegg <93870106+EdiAnderegg@users.noreply.github.com>
2024-10-12 20:09:28 +0200

    update to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FeaturesModule } from './features/features.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { OptionsComponent } from './options/options.component';
 
-@NgModule({
-  declarations: [AppComponent, OptionsComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    ReactiveFormsModule,
-    FeaturesModule,
-    FontAwesomeModule,
-    ModalModule.forRoot(),
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, OptionsComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        NgbModule,
+        ReactiveFormsModule,
+        FeaturesModule,
+        FontAwesomeModule,
+        ModalModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
   constructor() {}
 }
